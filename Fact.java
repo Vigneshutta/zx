3c2968b57c0ef412994ea9e6049780647cc5ae17
Vigneshutta <gchitradevi97@gmail.com>
2017-07-07 15:21:22 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Fact
+{
+    public static void main(String arg[])
+    {
+         int n=5;
+          int result=1;
+        for(int i =1;i<=n;i++)
+        {
+            result=result*i;
+         }   
+           
+        System.out.println("The factorial of 5 is"+result);
+    
+    }
+}
